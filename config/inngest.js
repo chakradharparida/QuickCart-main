f761427b8960cec0b55c1735d248618e220721c9
chakradharparida <chakradharp114@gmail.com>
2025-05-13 18:32:29 +0530

    completed

--- config/inngest.js
+++ config/inngest.js
@@ -30,7 +30,7 @@ export  const syncUserUpdation = inngest.createFunction(
     {
         id:'update-user-from-clerk'
     },
-    {event :'clerk/user.updated'},
+        {event :'clerk/user.updated'},
     async ({event}) => {
         const { id, first_name, last_name,email_addresses, image_url } = event.data 
         const userData = {
@@ -45,15 +45,16 @@ export  const syncUserUpdation = inngest.createFunction(
 )
 
 
-//Innght function to delete user data from a database
+//Innght function to delete user data from database
 
 export const syncUserDeletion = inngest.createFunction(
     {
-        id:'delete-user-from-clerk'
+        id:'delete-user-with-clerk'
     },
     {event:'clerk/user.deleted'},
     async ({event}) => {
-        const { id } = event.data 
+        const { id } = event.data
+         
         await connectDB()
         await User.findByIdAndDelete(id)
     }
