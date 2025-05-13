f4e4e6f36e19408cd1b649d2617643050941b526
chakradharparida <chakradharp114@gmail.com>
2025-05-13 09:18:41 +0530

    changes done

--- config/inngest.js
+++ config/inngest.js
@@ -1,8 +1,10 @@
-import { inngest } from "inngest";
+import { Inngest } from "inngest";
 import connectDB from "./db";
+import User from "./models/User"; 
+
 
 // Create a client to send and receive events
-export const inngest = new inngest({ id: "quickcart-next" });
+export const inngest = new Inngest({ id: "quickcart-next" });
 
 //Innght function to save user data to a database 
 export const syncUserCreation = inngest.createFunction(
@@ -34,7 +36,7 @@ export  const syncUserUpdation = inngest.createFunction(
         const userData = {
             _id:id,
             email:email_addresses[0].email_address,
-            name: first_name + '  ' + last_name,
+            name: first_name + ' ' + last_name,
             imageUrl:image_url
         }
         await connectDB()
